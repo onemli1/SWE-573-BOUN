e9a784f6446768decca013c18c46ef3ac6aa1fe1
SULTAN TURKER ONEMLI <90937984+onemli1@users.noreply.github.com>
2022-01-09 07:05:21 +0300

    Add files via upload

---
+++ apiCalls.js
@@ -0,0 +1,5 @@
+const signup = (BODY) => {
+
+    return axios('/api/1.0/users', body)
+
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './bootstrap-override.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import UserPage from './pages/UserPage';
+import UserSignupPage from './pages/UserSignUpPage';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UserSignUpPage/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
